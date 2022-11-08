f0ccb5382823e7bc7608b70841cbedc03d3ac6ba
陈文希 <lianlianyi@vip.qq.com>
2022-11-08 15:09:42 +0800

    pi4j 控制 oled

--- src/main/java/cc/chenwenxi/mypi4j/Mypi4jApplication.java
+++ src/main/java/cc/chenwenxi/mypi4j/Mypi4jApplication.java
@@ -2,17 +2,14 @@ package cc.chenwenxi.mypi4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Mypi4jApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Mypi4jApplication.class, args);
-
-		System.out.println("启动oled");
-		OledKit oled = new OledKit(0, 0);
-		oled.start();
-		oled.text("ok");
 	}
 
 }
